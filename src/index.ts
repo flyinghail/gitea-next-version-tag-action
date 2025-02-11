a65595fd59839eeeda460437af77247757af90b6
FlyingHail <flyinghail@spacenation.online>
2025-02-11 15:37:51 +0800

    increament next version

--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,147 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { Webhooks } from '@octokit/webhooks';
+import {exec, getExecOutput} from "@actions/exec";
+import * as semver from "semver";
+import type { PullRequestEvent } from "@octokit/webhooks-types";
 
-const fragments = ['major', 'feature', 'bug'];
-const types = ['beta', 'alpha'];
-
-function versionRegex() {
-    const prefix = core.getInput('prefix');
-    return new RegExp(`${prefix}(\\d+)\\.(\\d+)\\.(\\d+)`);
+function versionPrefix() {
+    return core.getInput('prefix') || "v";
 }
 
-function getReleaseType() {
-    const labels = getLabels();
-
-    return labels
-        .filter(l => types.includes(l))
-        .sort(l => types.indexOf(l))
-        .reverse()[0] ?? 'release';
+function getMajorLabel() {
+    return core.getInput('major-label') || 'major'
 }
 
-async function findLastVersion(): Promise<string | undefined> {
-    const token = core.getInput('token');
+function getMinorLabel() {
+    return core.getInput('minor-label') || 'minor'
+}
 
-    const { data } = await github.getOctokit(token).repos.listTags({ ...github.context.repo });
-    console.log('Found tags', data.map(t => t.name))
+function getPatchLabel() {
+    return core.getInput('patch-label') || 'patch'
+}
 
-    const regex = versionRegex();
-    const versions = data.map(t => t.name.match(regex)).filter(v => !!v) as RegExpMatchArray[];
+function getFragments() {
+    return [getMajorLabel(), getMinorLabel(), getPatchLabel()]
+}
 
-    const s = (a: RegExpMatchArray) => {
-        const [, m, r, b] = a.map(v => Number.parseInt(v));
-        return m * 1000000 + r * 1000 + b
-    };
-    const latest = versions.sort((a, b) => s(a) - s(b))[0];
+export async function getTags() {
+    await exec(`git`, [`fetch`, `--tags`])
+    const output = await getExecOutput(`git`, [`tag`, `-l`])
 
-    return data[versions.indexOf(latest)]?.name;
+    if (output.exitCode != 0) {
+        console.log(`Fetch tags failed, ${output.stderr}`);
+        throw new Error(`No tags found, ${output.stderr}`);
+    }
+
+
+    return output.stdout.split(`\n`);
+}
+
+function getSemverVersion(tag: string) {
+    const prefix = versionPrefix()
+
+    if (tag.startsWith(prefix)) {
+        tag = tag.substring(prefix.length);
+    }
+
+    const semverVer = semver.clean(tag)
+    if (semverVer && !semverVer.includes('-')) {
+        return semverVer
+    }
+
+    return null
+}
+
+async function findLastVersion() {
+    const tags = await getTags();
+
+    const semverTags: string[] = []
+    if (tags.length > 0) {
+        tags.forEach(tag => {
+            const semverTag = getSemverVersion(tag)
+            if (semverTag) {
+                semverTags.push(semverTag)
+            }
+        })
+    }
+
+    if (!semverTags || semverTags.length === 0) {
+        const fallback = semver.clean(core.getInput('fallback')) || '0.0.0'
+        console.log(`No tags found, using ${fallback}`);
+        return fallback;
+    }
+
+    // Filter out all the prerelease tags and sort them
+    semverTags.sort(semver.compare);
+    return semverTags[semverTags.length - 1]
 }
 
 function increment(version: string, by: string) {
-    const match = version.match(versionRegex())
-    const prefix = core.getInput('prefix');
+    // Split the latest tag into its parts
+    const current = semver.parse(version)
 
-    if (!match) throw new Error(`'${version}' is not a valid version`)
+    if (current) {
+        // Increment the version
+        switch (by) {
+            case getMajorLabel():
+                return semver.inc(current, 'major') || ''
+            case getMinorLabel():
+                return semver.inc(current, 'minor') || ''
+            case getPatchLabel():
+                return semver.inc(current, 'patch') || ''
+        }
+    }
 
-    const inc = fragments.indexOf(by);
-    if (inc < 0) throw new Error(`'${by} is not a valid fragment`)
-    const v = match.slice(1, match.length).map(d => Number.parseInt(d));
-
-    const incremented = v.map((n, i) => {
-        if (i === inc) return n + 1;
-        if (i > inc) return 0;
-        else return n;
-    })
-
-    return prefix + incremented.join('.');
+    return ''
 }
 
 function getLabels(): string[] {
-    const { eventName } = github.context;
-
-    switch (eventName) {
-        case 'pull_request': {
+    switch (github.context.eventName) {
+        case 'pull_request':
             console.log('Triggered on pull request');
-            const payload = github.context.payload as Webhooks.WebhookPayloadPullRequest;
+            const payload = github.context.payload as PullRequestEvent;
             return payload.pull_request.labels.map(l => l.name);
-        }
-        case 'repository_dispatch': {
+        case 'repository_dispatch':
             console.log('Triggered on repository dispatch');
             const { client_payload } = github.context.payload as any;
             return [client_payload.fragment, client_payload.type].filter(l => !!l);
-        }
-        default: return [];
+        default:
+            return [];
     }
 }
 
 function findFragment() {
     const labels = getLabels();
-
+    const fragments = getFragments()
     return labels
         .filter(l => fragments.includes(l))
         .sort(l => fragments.indexOf(l))
-        .reverse()[0];
+        .reverse()[0]
 }
 
-async function lastVersion() {
+async function getLastVersion() {
     const input = core.getInput('last-version');
-    if(input) return input;
-    else return findLastVersion();
+    if(input) {
+        return getSemverVersion(input);
+    }
+
+    return findLastVersion();
 }
 
 async function run() {
+    const lastVersion = await getLastVersion()
 
-    const last_version = await lastVersion();
-
-    if (last_version) {
-        const fragment = findFragment() ?? core.getInput('default-fragment')
+    let next = ''
+    if (lastVersion) {
+        const fragment = findFragment() || getPatchLabel()
         console.log('Using version fragment', fragment)
-        console.log('Found last version', last_version)
-        const next = increment(last_version, fragment);
-        core.setOutput('next', next)
-    } else {
-        const prefix = core.getInput('prefix');
-        const fallback = core.getInput('fallback') || `${prefix}1.0.0`
-        console.log('Did not find last version, using fallback', fallback)
-        if (!versionRegex().test(fallback)) throw new Error(`Fallback '${fallback}' is not a valid version`)
-        core.setOutput('next', fallback);
+        console.log('Found last version', lastVersion)
+        next = increment(lastVersion, fragment)
     }
 
-    const type = getReleaseType();
-    console.log(`Using release type '${type}'`)
-    core.setOutput("type", type);
-
+    const prefix = versionPrefix()
+    core.setOutput('next', `${prefix}${next}`)
+    core.setOutput("latest", `${prefix}${lastVersion}`)
 }
 
 run().catch(e => core.setFailed(e.message));
